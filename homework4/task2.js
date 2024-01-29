454b152f7a24b57cb71d42a413a27abe274b1c1d
litserkovna <39171450+litserkovna@users.noreply.github.com>
2024-01-29 19:27:48 +0100

    Home task number 4

---
+++ homework4/task2.js
@@ -0,0 +1,20 @@
+const averageGrade = 75;
+switch (true) {
+    case averageGrade < 60:
+        console.log("Unsatisfactory");
+        break;
+    case averageGrade >= 60 && averageGrade <= 70:
+        console.log("Satisfactory");
+        break;
+    case averageGrade >= 71 && averageGrade <= 80:
+        console.log("Good");
+        break;
+    case averageGrade >= 81 && averageGrade <= 90:
+        console.log("Very good");
+        break;
+    case averageGrade >= 91 && averageGrade <= 100:
+        console.log("Perfect");
+        break;
+    default:
+        console.log("Invalid");
+}
