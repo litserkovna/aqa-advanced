18a02f86bb5d5a34685da5c82a0df8165e0c0894
litserkovna <39171450+litserkovna@users.noreply.github.com>
2024-02-01 16:32:14 +0100

    Homework5

---
+++ homework5/task1.js
@@ -0,0 +1,18 @@
+//Function declaration
+function calculateRectangleArea(width, height) {
+    const area = width * height;
+    return area;
+}
+console.log(calculateRectangleArea(5,10));
+//Function expression
+const calculateRectangleAreaExpression = function(width, height) {
+    const area = width * height;
+    return area;
+};
+console.log(calculateRectangleAreaExpression(5,5));
+//Arrow function expression
+const calculateRectangleAreaArrow = (width, height) => {
+    const area = width * height;
+    return area;
+};
+console.log(calculateRectangleAreaArrow(1,5)); 
