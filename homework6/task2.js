84f35974f88a53fdd83454eb58d200cba18812b2
litserkovna <39171450+litserkovna@users.noreply.github.com>
2024-02-10 15:18:26 +0100

    Added console.log and removed my test function

--- homework6/task2.js
+++ homework6/task2.js
@@ -1,10 +1,5 @@
-function countToZero(num) {
-    for (let a = num; a >= 0; a--) {
-        console.log(a);
-    }
-}
-countToZero(5);
 function recursiveCounter(num) {
+    console.log(num);
     if (num > 0) {
         recursiveCounter(num - 1);
     }
