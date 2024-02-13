a56a8de16c351ef6a872e4bc9653e21cea28b18f
litserkovna <39171450+litserkovna@users.noreply.github.com>
2024-02-13 11:45:00 +0100

    Added homework 8

---
+++ homework8/task1.js
@@ -0,0 +1,6 @@
+const student = {}
+    student.firstName = "Lisa",
+    student.lastName = "Tserkovna"
+    student.age = 24,
+    student.courses = ["photograpy", "IT", "math"]
+console.log(student);
